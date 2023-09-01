5acd895fc87a9315e4f9f5a49d064f55f8836266
TheViking733n <anant733n@gmail.com>
2023-09-02 01:41:28 +0530

    created Room model

--- room/models.py
+++ room/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Room(models.Model):
+    id = models.CharField(max_length=10, primary_key=True)
+    owner = models.EmailField()
+    players = models.JSONField()
+    config = models.JSONField()
+    waiting = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
